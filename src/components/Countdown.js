ee1ee69d19d0ba97c293d89ee28ee78b07e48718
shazy89 <erdo.shazy123@gmail.com>
2021-08-23 11:36:35 -0400

    count the minutes and seconts

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import { fontSizes, paddingSizes } from "../utils/sizes";
+import { colors } from "../utils/colors";
 const minutesToMillis = (min) => min * 1000 * 60;
 //const formatTime
 const Countdown = ({ minutes = 20, isPaused }) => {
-  return <Text>Countdown Timer goes here</Text>;
+  const [millis, setMillis] = useState(minutesToMillis(1));
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
+
+  return <Text style={styles.text}>{millis}</Text>;
 };
 
 export default Countdown;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSizes.xxxxl,
+    fontWeight: "bold",
+    padding: paddingSizes.lg,
+    backgroundColor: "rgba(166, 255, 0, 0.274)",
+    color: colors.white
+  }
+});
--- src/utils/sizes.js
+++ src/utils/sizes.js
@@ -3,19 +3,19 @@ export const fontSizes = {
   md: 16,
   lg: 24,
   xl: 32,
-  xxxxl: 80
+  xxxxl: 60
 };
 export const paddingSizes = {
   sm: 8,
   md: 16,
   lg: 24,
   xl: 32,
-  xxxxl: 80
+  xxxxl: 60
 };
 export const marginSizes = {
   sm: 8,
   md: 16,
   lg: 24,
   xl: 32,
-  xxxxl: 80
+  xxxxl: 60
 };
