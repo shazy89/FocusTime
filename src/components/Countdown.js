0d33c459878174a75ccf64fa4ad1b168e43dd797
shazy89 <erdo.shazy123@gmail.com>
2021-08-24 12:27:54 -0400

    working countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { fontSizes, paddingSizes } from "../utils/sizes";
 import { colors } from "../utils/colors";
 const minutesToMillis = (min) => min * 1000 * 60;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+//const formatIncome = (amount) => (amount > 0 ? `$ ${amount}.00` : null);
 //const formatTime
 const Countdown = ({ minutes = 20, isPaused }) => {
-  const [millis, setMillis] = useState(minutesToMillis(1));
+  const interval = useRef(null);
+
+  const countDown = () => {
+    setMillis((time) => {
+      if (time === 0) {
+        return time;
+      }
+      const timeLeft = time - 1000;
+      return timeLeft;
+    });
+  };
+  useEffect(() => {
+    if (isPaused) return;
+    interval.current = setInterval(countDown, 1000);
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
+
+  const [millis, setMillis] = useState(minutesToMillis(minutes));
   const minute = Math.floor(millis / 1000 / 60) % 60;
   const seconds = Math.floor(millis / 1000) % 60;
 
-  return <Text style={styles.text}>{millis}</Text>;
+  return (
+    <>
+      <Text style={styles.text}>
+        {formatTime(minute)}: {formatTime(seconds)}
+      </Text>
+    </>
+  );
 };
 
 export default Countdown;
@@ -19,7 +44,7 @@ const styles = StyleSheet.create({
     fontSize: fontSizes.xxxxl,
     fontWeight: "bold",
     padding: paddingSizes.lg,
-    backgroundColor: "rgba(166, 255, 0, 0.274)",
+    backgroundColor: "rgba(0, 140, 255, 0.274)",
     color: colors.white
   }
 });
--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { fontSizes } from "../../utils/sizes";
 import { colors, marginSizes } from "../../utils/colors";
 import Countdown from "../../components/Countdown";
+import RoundedButton from "../../components/RoundedButton";
 const Timer = ({ focusSubject }) => {
+  const [isStarted, setIsStarted] = useState(false);
   return (
     <View style={styles.container}>
-      <Countdown />
-      <Text style={styles.title}>Focusing on:</Text>
-      <Text style={styles.task}>{focusSubject}</Text>
+      <View style={styles.countdown}>
+        <Countdown isPaused={!isStarted} />
+      </View>
+      <View style={styles.theRest}>
+        <Text style={styles.title}>Focusing on:</Text>
+        <Text style={styles.task}>{focusSubject}</Text>
+      </View>
+      <View style={styles.buttonWraper}>
+        {!isStarted ? (
+          <RoundedButton
+            title="start"
+            size={70}
+            onPress={() => setIsStarted(true)}
+          />
+        ) : (
+          <RoundedButton
+            title="pause"
+            size={70}
+            onPress={() => setIsStarted(false)}
+          />
+        )}
+      </View>
     </View>
   );
 };
@@ -30,5 +51,17 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontWeight: "400",
     textAlign: "center"
+  },
+  countdown: {
+    flex: 1
+  },
+  theRest: {
+    flex: 1
+  },
+  buttonWraper: {
+    flex: 0.3,
+    padding: 15,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
